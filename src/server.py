bd1f55255358ec9802afb3a0ecce3287abb8093c
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-03 09:48:08 -0700

    cleanup of server during code review

--- src/server.py
+++ src/server.py
@@ -6,40 +6,48 @@ def listen():
     port = 3000
     address = ('127.0.0.1', port)
 
-    print("--- Starting server on port {} at {}---".format(port, dt.now()))
-
     sock = socket.socket(
         socket.AF_INET,
         socket.SOCK_STREAM,
         socket.IPPROTO_TCP
     )
     sock.bind(address)
-    sock.listen(1)
 
-    while True:
+    try:
+        sock.listen(1)
+
+        print("--- Starting server on port {} at {}---".format(port, dt.now().strftime('%H:%M:%S %d-%m-%y')))
+        
         conn, addr = sock.accept()
-        buffer_length = 8
-        while True:
-            message = conn.recv(buffer_length)
-            data = message.decode('utf8')
 
-            try:
-                if data == 'quit':
-                    print("--- Stopping server on port {} at {}---".format(port, dt.now()))
-                    conn.close()
-                    sock.close()
-                    quit()
-                elif len(message) < buffer_length:
-                    # message_complete = True
-                    break
-                else:
-                    conn.sendall(data.encode('utf8'))
-                    print(data)
-            except KeyboardInterrupt:
-                print("--- Stopping server on port {} at {}---".format(port, dt.now()))
-                conn.close()
-                sock.close()
-                quit()
+        buffer_length = 8
+
+        message_complete = False
+        
+        message = b''
+        while not message_complete:
+            part = conn.recv(buffer_length)
+            message += part
+            if len(part) < buffer_length:
+                break
+        
+        message = message.decode('utf8')
+        print ('{} Echoed: {}'.format(dt.now().strftime('%H:%M:%S %d-%m-%y'), message))
+
+        conn.sendall(message.encode('utf8'))
+
+    except KeyboardInterrupt:
+        try:
+            conn.close()
+        except NameError:
+            pass
+
+        sock.close()
+        print("--- Stopping server on port {} at {}---".format(port, dt.now().strftime('%H:%M:%S %d-%m-%y')))
+    
+    conn.close()
+    sock.close()
+    print("--- Stopping server on port {} at {}---".format(port, dt.now().strftime('%H:%M:%S %d-%m-%y')))
     
 
 if __name__ == "__main__":
@@ -48,12 +56,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         pass
 
-
-
-# conn.sendall(message.encode('utf8'))
-
-# while not message_complete:
-#     part = conn.recv(buffer_length)
-#     print(part.decode('utf8'))
-#     if ln(part) < buffer_length:
-#         break
